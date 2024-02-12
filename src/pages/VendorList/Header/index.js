7be9a4b593b74ef68088ba346a834b837f333cc3
parnazphd <parnazphd@gmail.com>
2024-02-12 10:32:15 +0330

    add alt for image

--- src/pages/VendorList/Header/index.js
+++ src/pages/VendorList/Header/index.js
@@ -7,9 +7,9 @@ const Index = () => {
 
     return (
         <HeaderWrapper>
-            <OverlayImage src="https://m.snappfood.ir/9a8c9c8510acd3f05587cdf638a1f122.svg"/>
-            <BackgroundImage src={vendor.backgroundImage}/>
-            <ProfileImage src={vendor.defLogo}/>
+            <OverlayImage src="https://m.snappfood.ir/9a8c9c8510acd3f05587cdf638a1f122.svg" alt={vendor.title}/>
+            <BackgroundImage src={vendor.backgroundImage} alt={vendor.title}/>
+            <ProfileImage src={vendor.defLogo} alt={vendor.title}/>
         </HeaderWrapper>
     )
 
